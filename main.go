20a31a73e1022a662d6204ab2b2c5dd44692e5d6
sachithnim <nimendras07@gmail.com>
2025-03-27 20:51:35 +0530

    Add logging middleware to the main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"book-api-assignment/handlers"
+	"book-api-assignment/middleware"
 	"book-api-assignment/routes"
 	"log"
 	"net/http"
@@ -10,7 +11,10 @@ import (
 func main() {
 	router := routes.SetupRoutes()
 
-	// Ensure this line is added to handle the root route.
+	// Add the logging middleware
+	router.Use(middleware.Logger)
+
+	// root route.
 	router.HandleFunc("/", handlers.RootHandler).Methods("GET")
 
 	log.Println("Server running on port 8000")
